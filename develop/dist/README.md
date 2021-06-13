c412fc2819c94437fbf1f16c20398e1af485f27a
Jeremy's commits <jeremyhurst16@gmail.com>
2021-06-13 17:16:04 -0400

    generating README from index, returning correct parameters

---
+++ develop/dist/README.md
@@ -0,0 +1,33 @@
+#undefined
+  ## Description
+  azdfrgazsdrgfaz
+  *HTML,CSS,ES6,BOOTSTRAP,NODE
+
+  ### What did I Learn?
+  SEAFSEF
+
+  ### GitHub Link
+  fgb
+
+  ## Table of Contents
+  [Installation](#installation)
+  [Contribution](#contribution)
+  [Tests](#tests)
+  [Licenses](#license)
+  [Email](#email)
+
+  ### Installation
+  undefined
+
+  ### Contribution
+  undefined
+
+  ### Testing
+  undefined
+
+  ### Licensing 
+  undefined
+
+  ### Connect With Me
+  For any and all questions please email me @undefined or fgb Thank you!
+  
--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-
+const fs = require('fs');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -13,8 +13,58 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-`;
+  return `#${data.name}
+  ## Description
+  ${data.description}
+  *${data.language}
+
+  ## What did I Learn?
+  ${data.learning}
+
+  ## GitHub Link
+  ${data.link}
+
+  ## Table of Contents
+  (#installation)
+  (#contribution)
+  (#tests)
+  (#license)
+  (#email)
+
+  ## Installation
+  ${data.installation}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Testing
+  ${data.tests}
+
+  ## Licensing 
+  ${data.licenses}
+
+  ## Connect With Me
+  For any and all questions please email me @${data.email} or ${data.link} Thank you!
+  `;
 }
 
-module.exports = generateMarkdown;
+const writeFile = fileContent => {
+  return new Promise((resolve, reject) => {
+      fs.writeFile('./dist/README.md', fileContent, err => {
+          // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+          if (err) {
+              reject(err);
+              // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+              return;
+          }
+
+          // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+          resolve({
+              ok: true,
+              message: 'README is created!'
+          });
+      });
+  });
+};
+
+module.exports = { generateMarkdown, writeFile };
